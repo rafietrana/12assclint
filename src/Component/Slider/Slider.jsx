e160d346900c4dd913d1fff61960229dafba0328
rafietrana <bdmail674@gmail.com>
2024-06-03 13:50:24 +0600

    add this code

--- src/Component/Slider/Slider.jsx
+++ src/Component/Slider/Slider.jsx
@@ -2,51 +2,46 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const Slider = () => {
-  const { data: activeBannerData=[] } = useQuery({
-    queryKey: ['activeBanner'],
+  const { data: activeBannerData = [] } = useQuery({
+    queryKey: ["activeBanner"],
     queryFn: () =>
-      axios('http://localhost:5000/getactivebanner')
-        .then(res =>{
-          return res.data
-        })
-  })
+      axios("http://localhost:5000/getactivebanner").then((res) => {
+        return res.data;
+      }),
+  });
 
-
-
-  console.log('alhamdulillah active bannerdata is mashallah', activeBannerData);
+  console.log("alhamdulillah active bannerdata is mashallah", activeBannerData);
 
   const getStyledBannerTitle = (title) => {
-    if (!title) return '';
+    if (!title) return "";
 
-    const words = title.split(' ');
-    if (words.length < 3) return title;  
-    console.log('alhamdulillah word is ', words);
+    const words = title.split(" ");
+    if (words.length < 3) return title;
+    console.log("alhamdulillah word is ", words);
 
     return (
       <>
-        {words.slice(0, 2).join(' ')}{' '}
-        <span className="text-green-500">{words[2]}</span>{' '}
-        {words.slice(3).join(' ')}
+        {words.slice(0, 2).join(" ")}{" "}
+        <span className="text-green-500">{words[2]}</span>{" "}
+        {words.slice(3).join(" ")}
       </>
     );
   };
-
 
   return (
     <div className="bg-[#E5F8FC]   h-[600px]  ">
       <div className="w-10/12 mx-auto lg:flex  justify-between   items-center">
         <div className="space-y-3 w-full md:w-1/2 ">
           <p className="font-Outfit uppercase bg-green font-bold text-[#0DD07D]">
-            {activeBannerData?.bannername}
+            For <span className="text-blue-500"> {activeBannerData?.couponrate} %</span> Descount Apply <span className="text-blue-500"> {activeBannerData?.couponcode}</span>
           </p>
           <p className="text-7xl font-bold font-Outfit space-y-3 ">
             {/* Caring for <span className="text-[#0DD07D]">Health</span> <br />
             Caring for You */}
- {getStyledBannerTitle(activeBannerData?.bannertitle)}
+            {getStyledBannerTitle(activeBannerData?.bannertitle)}
           </p>
           <p className="font-Outfit text-[#788094]">
             {activeBannerData?.description}
- 
           </p>
           <div className="flex gap-2 py-5  ">
             <div>
