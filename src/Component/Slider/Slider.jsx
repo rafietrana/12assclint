b86d015f8205e8d2483da06292ddaaf8ea8229fb
rafietrana <bdmail674@gmail.com>
2024-06-03 13:36:15 +0600

    add this code

--- src/Component/Slider/Slider.jsx
+++ src/Component/Slider/Slider.jsx
@@ -1,18 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const Slider = () => {
+  const { data: activeBannerData=[] } = useQuery({
+    queryKey: ['activeBanner'],
+    queryFn: () =>
+      axios('http://localhost:5000/getactivebanner')
+        .then(res =>{
+          return res.data
+        })
+  })
+
+
+
+  console.log('alhamdulillah active bannerdata is mashallah', activeBannerData);
+
+  const getStyledBannerTitle = (title) => {
+    if (!title) return '';
+
+    const words = title.split(' ');
+    if (words.length < 3) return title;  
+    console.log('alhamdulillah word is ', words);
+
+    return (
+      <>
+        {words.slice(0, 2).join(' ')}{' '}
+        <span className="text-green-500">{words[2]}</span>{' '}
+        {words.slice(3).join(' ')}
+      </>
+    );
+  };
+
+
   return (
     <div className="bg-[#E5F8FC]   h-[600px]  ">
       <div className="w-10/12 mx-auto lg:flex  justify-between   items-center">
         <div className="space-y-3 w-full md:w-1/2 ">
           <p className="font-Outfit uppercase bg-green font-bold text-[#0DD07D]">
-            24/7 EMERGENCY SERVICE
+            {activeBannerData?.bannername}
           </p>
           <p className="text-7xl font-bold font-Outfit space-y-3 ">
-            Caring for <span className="text-[#0DD07D]">Health</span> <br />
-            Caring for You
+            {/* Caring for <span className="text-[#0DD07D]">Health</span> <br />
+            Caring for You */}
+ {getStyledBannerTitle(activeBannerData?.bannertitle)}
           </p>
           <p className="font-Outfit text-[#788094]">
-            A brief statement outlining the purpose and mission of the clinic.
-            This can include the commitment to patient care, community health
+            {activeBannerData?.description}
+ 
           </p>
           <div className="flex gap-2 py-5  ">
             <div>
@@ -28,10 +62,10 @@ const Slider = () => {
             </div>
           </div>
         </div>
-        <div className="md:w-1/2 hidden lg:block w-full ">
+        <div className="md:w-1/2 hidden lg:block w-full  ">
           <img
             className="h-[600px] object-cover overflow-hidden"
-            src="https://i.ibb.co/xSX90Gz/hero-2-1.png"
+            src={activeBannerData?.bannerimg}
             alt=""
           />
         </div>
